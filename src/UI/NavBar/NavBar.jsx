626502a759b9271c428cd5348d79c8bc7418d897
KobiManuel <Emmakobi91@gmail.com>
2023-01-07 23:20:58 +0100

    feat(New import): imported react-icons

--- src/UI/NavBar/NavBar.jsx
+++ src/UI/NavBar/NavBar.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
+import { AiOutlineShoppingCart } from 'react-icons/ai'
 
 const NavBar = () => {
   return (
     <div className=' bg-white body-font font-poppins'>
-       <div className=' max-w-[1100px] flex bg-white items-center gap-9 h-24 mx-auto'>
+       <div className=' max-w-[1100px] flex bg-white items-center justify-between gap-9 h-24 mx-auto'>
        <div className="px-2 w-fit flex flex-row">
            <h3 className=' text-2xl font-semibold text-[var(--primary)]'>React <span className='text-[var(--secondary)]'>Meals</span></h3>
           </div>
           <ul id="unorderedList"
             className="hidden md:flex gap-7 text-sm text-[var(--secondary)]">
-            <li className=' border-b-2 border-b-[var(--primary)] font-semibold text-[16px]'><a>Home</a></li>
-            <li className=' text-[#868e96] text-[16px]'><a>Menu</a></li>
-            <li className=' text-[#868e96] text-[16px]'><a>How it works</a></li>
-            <li className=' text-[#868e96] text-[16px]'><a>About</a></li>
-            <li className=' text-[#868e96] text-[16px]'><a>FAQS</a></li>
-            <li className=' text-[#868e96] text-[16px]'><a>Contact</a></li>
+            <li className=' border-b-2 border-b-[var(--primary)] font-semibold text-[15px]'><a>Home</a></li>
+            <li className=' text-[#868e96] text-[15px]'><a>Menu</a></li>
+            <li className=' text-[#868e96] text-[15px]'><a>How it works</a></li>
+            <li className=' text-[#868e96] text-[15px]'><a>About</a></li>
+            <li className=' text-[#868e96] text-[15px]'><a>FAQS</a></li>
+            <li className=' text-[#868e96] text-[15px]'><a>Contact</a></li>
           </ul>
+          <div className='w-fit'>
+            <button><AiOutlineShoppingCart /> </button>
+          </div>
        </div>
     </div>
   )

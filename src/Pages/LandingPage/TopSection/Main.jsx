836921504cd96782417e9d241f6692581159a3c1
KobiManuel <Emmakobi91@gmail.com>
2023-01-08 18:37:10 +0100

    feat(User-interface): populated text for hero section

--- src/Pages/LandingPage/TopSection/Main.jsx
+++ src/Pages/LandingPage/TopSection/Main.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import Wrapper from '../../../UI/Wrapper/Wrapper'
+import hero_bg from './images/Hero2.jpg'
 
 const TopSection = () => {
   return (
     <Wrapper style={{
-      backgroundImage: "url(./images/Hero2,jpg)"
+      backgroundImage: "url(" + hero_bg + ")",
+      backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center center",
+        width: "100%",
+        maxWidth: "100%",
+        height: "fit-content",
     }}>
+      <header className=' max-w-[1100px] mx-auto body-font font-poppins'>
+        <h3>A chef in every tasty meal box</h3>
+        <p>Get pre-portioned ingrdients for Gusto Meals Kits best recipes delivered direct to your door!</p>
+
+      </header>
         
     </Wrapper>
   )

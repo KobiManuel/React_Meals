5742da36e1007d8f3d896e66378eec6063fe0edc
KobiManuel <Emmakobi91@gmail.com>
2023-01-09 19:25:51 +0100

    refactor: updated mealcardinfo

--- src/Pages/LandingPage/MealsSection/MealCardInfo.jsx
+++ src/Pages/LandingPage/MealsSection/MealCardInfo.jsx
@@ -6,6 +6,22 @@ import bowl5 from "./images/bowl5.png";
 import bowl6 from "./images/box6.png";
 import bowl7 from "./images/box7.png";
 import bowl8 from "./images/box8png.png";
+import bowl9 from "./images/box9png.png";
+import bowl10 from "./images/box10.png";
+import bowl11 from "./images/box11.png";
+import bowl12 from "./images/box12.png";
+import bowl13 from "./images/box13.png";
+import bowl14 from "./images/box14.png";
+import bowl15 from "./images/box15.png";
+import bowl16 from "./images/box16.png";
+import bowl17 from "./images/box17.png";
+import bowl18 from "./images/box18.png";
+import bowl19 from "./images/box19.png";
+import bowl20 from "./images/box20.png";
+import bowl21 from "./images/box21.png";
+import bowl22 from "./images/box22.png";
+import bowl23 from "./images/box23.png";
+import bowl24 from "./images/box24.png";
 export const MealCardInfo = [
   {
     meal: bowl1,
@@ -47,4 +63,84 @@ export const MealCardInfo = [
     mealNameTop: "Grilled",
     mealNameBottom: "chicken parmesan",
   },
+  {
+    meal: bowl9,
+    mealNameTop: "Grilled",
+    mealNameBottom: "chicken parmesan",
+  },
+  {
+    meal: bowl10,
+    mealNameTop: "Grilled",
+    mealNameBottom: "chicken parmesan",
+  },
+  {
+    meal: bowl11,
+    mealNameTop: "Grilled",
+    mealNameBottom: "chicken parmesan",
+  },
+  {
+    meal: bowl12,
+    mealNameTop: "Grilled",
+    mealNameBottom: "chicken parmesan",
+  },
+  {
+    meal: bowl13,
+    mealNameTop: "Grilled",
+    mealNameBottom: "chicken parmesan",
+  },
+  {
+    meal: bowl14,
+    mealNameTop: "Grilled",
+    mealNameBottom: "chicken parmesan",
+  },
+  {
+    meal: bowl15,
+    mealNameTop: "Grilled",
+    mealNameBottom: "chicken parmesan",
+  },
+  {
+    meal: bowl16,
+    mealNameTop: "Grilled",
+    mealNameBottom: "chicken parmesan",
+  },
+  {
+    meal: bowl17,
+    mealNameTop: "Grilled",
+    mealNameBottom: "chicken parmesan",
+  },
+  {
+    meal: bowl18,
+    mealNameTop: "Grilled",
+    mealNameBottom: "chicken parmesan",
+  },
+  {
+    meal: bowl19,
+    mealNameTop: "Grilled",
+    mealNameBottom: "chicken parmesan",
+  },
+  {
+    meal: bowl20,
+    mealNameTop: "Grilled",
+    mealNameBottom: "chicken parmesan",
+  },
+  {
+    meal: bowl21,
+    mealNameTop: "Grilled",
+    mealNameBottom: "chicken parmesan",
+  },
+  {
+    meal: bowl22,
+    mealNameTop: "Grilled",
+    mealNameBottom: "chicken parmesan",
+  },
+  {
+    meal: bowl23,
+    mealNameTop: "Grilled",
+    mealNameBottom: "chicken parmesan",
+  },
+  {
+    meal: bowl24,
+    mealNameTop: "Grilled",
+    mealNameBottom: "chicken parmesan",
+  }
 ];

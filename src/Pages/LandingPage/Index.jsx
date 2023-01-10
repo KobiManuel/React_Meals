36f9b23090d75ab0494f944f0701fc230ab955c0
KobiManuel <Emmakobi91@gmail.com>
2023-01-10 22:09:47 +0100

    style(formatting): formatted document

--- src/Pages/LandingPage/Index.jsx
+++ src/Pages/LandingPage/Index.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import NavBar from '../../UI/NavBar/NavBar'
-import MealsSection from './MealsSection/Main'
-import Services from './Services/Main'
-import TopSection from './TopSection/Main'
+import React from "react";
+import NavBar from "../../UI/NavBar/NavBar";
+import MealsSection from "./MealsSection/Main";
+import Services from "./Services/Main";
+import TopSection from "./TopSection/Main";
 
 const Index = () => {
   return (
     <>
-    <NavBar />
-   <TopSection />
-   <MealsSection />
-   <Services />
+      <NavBar />
+      <TopSection />
+      <MealsSection />
+      <Services />
     </>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;

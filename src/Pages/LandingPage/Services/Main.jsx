a672c849cd3df1bbd55ae9e7d855b29181ea5da9
KobiManuel <Emmakobi91@gmail.com>
2023-01-10 15:18:45 +0100

    feat(New import): imported hero_bg

--- src/Pages/LandingPage/Services/Main.jsx
+++ src/Pages/LandingPage/Services/Main.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import Wrapper from '../../../UI/Wrapper/Wrapper'
+import hero_bg from '../TopSection/images/Hero2.jpg'
+
 
 const Services = () => {
   return (
-    <Wrapper>
+    <Wrapper style={{
+        backgroundImage: "url(" + hero_bg + ")",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center center",
+        width: "100%",
+        maxWidth: "100%",
+        height: "500px",
+      }}>
 
     </Wrapper>
   )

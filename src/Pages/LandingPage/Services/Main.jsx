f3b6b7115766e906c63e750b35854e40760e7bd1
KobiManuel <Emmakobi91@gmail.com>
2023-01-10 22:10:33 +0100

    style(formatting): did some formatting on services document

--- src/Pages/LandingPage/Services/Main.jsx
+++ src/Pages/LandingPage/Services/Main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Header from '../../../UI/Header/Header'
-import Wrapper from '../../../UI/Wrapper/Wrapper'
-import hero_bg from '../TopSection/images/Hero2.jpg'
-
+import React from "react";
+import Header from "../../../UI/Header/Header";
+import Wrapper from "../../../UI/Wrapper/Wrapper";
+import hero_bg from "../TopSection/images/Hero2.jpg";
 
 const Services = () => {
   return (
-    <Wrapper style={{
+    <Wrapper
+      style={{
         backgroundImage: "url(" + hero_bg + ")",
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
@@ -14,13 +14,13 @@ const Services = () => {
         width: "100%",
         maxWidth: "100%",
         height: "fit-content",
-      }}>
-        <div className=' max-w-[1100px] mx-auto'>
-          <Header title="why react meals?" text_color="text-white" />
-        </div>
-
+      }}
+    >
+      <div className=" max-w-[1100px] mx-auto">
+        <Header title="why react meals?" text_color="text-white" />
+      </div>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;

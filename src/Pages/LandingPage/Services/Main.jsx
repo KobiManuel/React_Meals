8340e111bf0c8d9a62914a6d0f76c89e42d47780
KobiManuel <Emmakobi91@gmail.com>
2023-01-11 23:19:10 +0100

    feat: created inner text

--- src/Pages/LandingPage/Services/Main.jsx
+++ src/Pages/LandingPage/Services/Main.jsx
@@ -19,7 +19,10 @@ const Services = () => {
       <div className=" max-w-[1100px] mx-auto">
         <Header title="why react meals?" text_color="text-white" />
         <div className=" w-full grid grid-cols-3">
-            
+          <div>
+            <span></span>
+            <p>Easy and Convenient</p>
+          </div>
         </div>
       </div>
     </Wrapper>

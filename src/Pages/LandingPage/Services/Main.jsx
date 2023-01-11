38c16d5bbe748226c61229b62b61ef9fd7d09c7c
KobiManuel <Emmakobi91@gmail.com>
2023-01-11 23:29:36 +0100

    feat: changed font-weight

--- src/Pages/LandingPage/Services/Main.jsx
+++ src/Pages/LandingPage/Services/Main.jsx
@@ -21,27 +21,27 @@ const Services = () => {
         <div className=" w-full grid grid-cols-3 body-font font-poppins gap-y-7">
           <div className=" flex flex-row text-white  justify-center items-center gap-4">
             <span className="w-[50px] h-[50px] rounded-full bg-white"></span>
-            <p className="text-2xl font-semibold">Easy and Convenient</p>
+            <p className="text-xl font-light">Easy and Convenient</p>
           </div>
           <div className=" flex flex-row text-white  justify-center items-center gap-4">
             <span className="w-[50px] h-[50px] rounded-full bg-white"></span>
-            <p className="text-2xl font-semibold">Easy and Convenient</p>
+            <p className="text-2xl font-light">Easy and Convenient</p>
           </div>
           <div className=" flex flex-row text-white  justify-center items-center gap-4">
             <span className="w-[50px] h-[50px] rounded-full bg-white"></span>
-            <p className="text-2xl font-semibold">Easy and Convenient</p>
+            <p className="text-2xl font-light">Easy and Convenient</p>
           </div>
           <div className=" flex flex-row text-white  justify-center items-center gap-4">
             <span className="w-[50px] h-[50px] rounded-full bg-white"></span>
-            <p className="text-2xl font-semibold">Easy and Convenient</p>
+            <p className="text-2xl font-light">Easy and Convenient</p>
           </div>
           <div className=" flex flex-row text-white  justify-center items-center gap-4">
             <span className="w-[50px] h-[50px] rounded-full bg-white"></span>
-            <p className="text-2xl font-semibold">Easy and Convenient</p>
+            <p className="text-2xl font-light">Easy and Convenient</p>
           </div>
           <div className=" flex flex-row text-white  justify-center items-center gap-4">
             <span className="w-[50px] h-[50px] rounded-full bg-white"></span>
-            <p className="text-2xl font-semibold">Easy and Convenient</p>
+            <p className="text-2xl font-light">Easy and Convenient</p>
           </div>
         </div>
       </div>

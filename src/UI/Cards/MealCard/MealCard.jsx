16f0e403341b4df13a20fb260766086f00742008
KobiManuel <Emmakobi91@gmail.com>
2023-01-11 18:49:49 +0100

    feat(User-interface): added overlay effect on MealCard component

--- src/UI/Cards/MealCard/MealCard.jsx
+++ src/UI/Cards/MealCard/MealCard.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import Button from '../../Button/Button'
 
 const MealCard = ({meal, mealNametop, mealNameBottom, id}) => {
   return (
     <div className='rounded-[2rem] bg-stone-100 w-[220] h-[334.67px] flex flex-col justify-center items-center'>
-      <div className='w-[100%] h-[220.06px]'><img src={meal} alt="/" className="mx-auto w-full h-full object-cover" /> </div>
+        <div className='relative overflow-hidden w-full h-full group'>
+      <div className='w-[100%] h-[220.06px]'><img src={meal} alt="/" className="mx-auto w-full h-full object-cover"/> </div>
        <div className='capitalize body-font font-poppins font-semibold text-[var(--secondary)] text-xl text-center'>
         <p>{mealNametop}</p>
         <p>{mealNameBottom}</p>
+       </div>
+       <div className="absolute h-full w-full bg-black/20 flex items-center justify-center -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300">
+        <Button>add to cart</Button>
+      </div>
        </div>
     </div>
   )

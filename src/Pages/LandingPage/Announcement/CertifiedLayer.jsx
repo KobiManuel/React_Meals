44a9f860b3751a7976fc9b5b3a67566aacd9214c
KobiManuel <Emmakobi91@gmail.com>
2023-01-12 19:47:01 +0100

    feat(New import): imported CertifiedLayer section

--- src/Pages/LandingPage/Announcement/CertifiedLayer.jsx
+++ src/Pages/LandingPage/Announcement/CertifiedLayer.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
-
+import Button from '../../../UI/Button/Button'
+import styles from '../../../UI/Button/TransparentBtn.module.css'
+import bowl from './images/true_transparent.png'
+import plant from './images/firstPlant.png'
+import signature from './images/signature.png'
 const CertifiedLayer = () => {
   return (
-    <div>CertifiedLayer</div>
+    <div  className='w-full relative'>
+         <img src={plant} alt="/" className='absolute' />
+        <div className='grid grid-cols-2'>
+        <div>
+            <img src={bowl} alt="/" />
+        </div>
+        <div className=' body-font font-poppins'>
+          <header>
+            <h3 className='text-4xl text-[var
+            (--secondary)] font-semibold' >We Deliver Anywhere in the Tri-State Area</h3>
+            <p className='text-[#868e96] text-sm'>Each React Meal is perfectly sized for one person to enjoy at 1 sitting. Our fully-prepared meals are delivered fresh, and ready to eat in 3 minutes</p>
+          </header>
+          <div>
+            <img src={signature} alt="/" />
+          </div>
+          <div className='flex flex-row gap-5'>
+            <Button>Contact us</Button>
+            <Button style={styles.button}>View menu</Button>
+          </div>
+        </div>
+        </div>
+    </div>
   )
 }
 
--- src/Pages/LandingPage/Announcement/Main.jsx
+++ src/Pages/LandingPage/Announcement/Main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Wrapper from "../../../UI/Wrapper/Wrapper";
+import CertifiedLayer from "./CertifiedLayer";
 import TopSection from "./TopSection";
 const Announcement = () => {
   return (
     <Wrapper bg_color=" bg-stone-100">
       <div className="max-w-[1100px] mx-auto flex flex-col gap-9">
        <TopSection />
+       <CertifiedLayer />
       </div>
     </Wrapper>
   );

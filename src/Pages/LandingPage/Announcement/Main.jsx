953375f6bd17d07b115196ab6ed2702c1248f07b
KobiManuel <Emmakobi91@gmail.com>
2023-01-12 16:32:58 +0100

    feat(New import): imported dash image

--- src/Pages/LandingPage/Announcement/Main.jsx
+++ src/Pages/LandingPage/Announcement/Main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Wrapper from '../../../UI/Wrapper/Wrapper'
+import dash from '../../../assets/dash.png'
 
 const Announcement = () => {
   return (
-    <Wrapper>
-        
+    <Wrapper bg_color="bg-white">
+    <div className='max-w-[1100px] mx-auto'>
+    <header className="capitalize body-font font-poppins text-center w-fit mx-auto -translate-y-[100%] ">
+          <h5 className="text-4xl text-[var
+            (--secondary)] font-semibold">
+            Favorite meals
+          </h5>
+          <img src={dash} alt="/" className="w-[300px] h-[60px] mt-[-9px]" />
+        </header>
+    </div>
     </Wrapper>
   )
 }

c370a1d51e9847c6ac9bd511d681602430d764a5
KobiManuel <Emmakobi91@gmail.com>
2023-01-12 18:45:57 +0100

    style: finished styling top-sectiom

--- src/Pages/LandingPage/Announcement/AnnouncementCard.jsx
+++ src/Pages/LandingPage/Announcement/AnnouncementCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AnnouncementCard = ({ image, top_text, bottom_text, position }) => {
   return (
-    <div className="bg-white py-9 h-[400px] rounded-[2rem] flex flex-col justify-center items-center gap-4 px-2">
+    <div className={`${position} bg-white py-9 h-[400px] rounded-[2rem] flex flex-col justify-center items-center gap-4 px-2`}>
       <figure className="w-[100%] h-fit">
         <img
           src={image}
--- src/Pages/LandingPage/Announcement/Main.jsx
+++ src/Pages/LandingPage/Announcement/Main.jsx
@@ -18,7 +18,7 @@ const Announcement = () => {
           </h5>
           <img src={dash} alt="/" className="w-[300px] h-[60px] mt-[-9px]" />
         </header>
-        <div className="grid grid-cols-3 gap-[7rem] px-10">
+        <div className="grid grid-cols-3 gap-[3rem] px-[90px]">
           <AnnouncementCard
             image={box1}
             top_text="choose your meals"
@@ -28,7 +28,7 @@ const Announcement = () => {
             image={box2}
             top_text="we deliver it to you"
             bottom_text="Choose your dates for delivery"
-            position="mt-5"
+            position="mt-9"
           />
           <AnnouncementCard
             image={box3}

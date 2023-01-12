57623ce1cd75ce2ff76b3e80c9602844ee546ba0
KobiManuel <Emmakobi91@gmail.com>
2023-01-12 18:55:59 +0100

    feat(New import): imported images

--- src/Pages/LandingPage/Announcement/Main.jsx
+++ src/Pages/LandingPage/Announcement/Main.jsx
@@ -1,10 +1,5 @@
 import React from "react";
 import Wrapper from "../../../UI/Wrapper/Wrapper";
-import dash from "../../../assets/dash.png";
-import AnnouncementCard from "./AnnouncementCard";
-import box1 from "./images/box1.png";
-import box2 from "./images/box2.png";
-import box3 from "./images/box3.png";
 import TopSection from "./TopSection";
 const Announcement = () => {
   return (
--- src/Pages/LandingPage/Announcement/TopSection.jsx
+++ src/Pages/LandingPage/Announcement/TopSection.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import dash from "../../../assets/dash.png";
+import AnnouncementCard from "./AnnouncementCard";
+import box1 from "./images/box1.png";
+import box2 from "./images/box2.png";
+import box3 from "./images/box3.png";
 
 const TopSection = () => {
   return (

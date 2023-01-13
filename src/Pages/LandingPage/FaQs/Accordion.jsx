bc67cf72fdd836fd6358a62408f1779a919f1d70
KobiManuel <Emmakobi91@gmail.com>
2023-01-13 23:28:47 +0100

    feat(formatting): formatted document

--- src/Pages/LandingPage/FaQs/Accordion.jsx
+++ src/Pages/LandingPage/FaQs/Accordion.jsx
@@ -1,23 +1,19 @@
-import React from 'react'
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+import React from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
 const Accordion = () => {
   return (
-    <div className='body-font font-poppins py-14 px-9 flex flex-row justify-center items-center w-fit h-fit bg-stone-100 hover:bg-[var(--secondary)] hover:rounderd-[2rem]'>
-        <span className=' text-4xl text-gray-300 font-semibold'>{index}</span>
-        <div className=' flex flex-col gap-6'>
-            <h5>{title}</h5>
-            <p>{description}</p>
-        </div>
-        <span>
-            {!open? (
-                <AiOutlineMinus size={25} />
-            ) : (
-                <AiOutlinePlus size={25}/>
-            )}
-        </span>
+    <div className="body-font font-poppins py-14 px-9 flex flex-row justify-center items-center w-fit h-fit bg-stone-100 hover:bg-[var(--secondary)] hover:rounderd-[2rem]">
+      <span className=" text-4xl text-gray-300 font-semibold">{index}</span>
+      <div className=" flex flex-col gap-6">
+        <h5>{title}</h5>
+        <p>{description}</p>
+      </div>
+      <span>
+        {!open ? <AiOutlineMinus size={25} /> : <AiOutlinePlus size={25} />}
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default Accordion
+export default Accordion;

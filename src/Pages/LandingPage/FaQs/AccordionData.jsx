75a4f03b48944eb0358adddfff74313c07562976
KobiManuel <Emmakobi91@gmail.com>
2023-01-13 23:30:23 +0100

    style(formatting): formatted AccordionData

--- src/Pages/LandingPage/FaQs/AccordionData.jsx
+++ src/Pages/LandingPage/FaQs/AccordionData.jsx
@@ -1,20 +1,26 @@
-export const AccordionData = [{
-   index: "01",
-   title: "How does ReactMeals kit delivery service work?",
-   description: "Our meal delivery service allows you to skip grocery shopping, HelloFresh delivers step-by-step recipes and frsh, preportioned ingredients"
-},
-{
+export const AccordionData = [
+  {
+    index: "01",
+    title: "How does ReactMeals kit delivery service work?",
+    description:
+      "Our meal delivery service allows you to skip grocery shopping, HelloFresh delivers step-by-step recipes and frsh, preportioned ingredients",
+  },
+  {
     index: "02",
     title: "How much do our meals cost?",
-    description: "Our meal delivery service allows you to skip grocery shopping, HelloFresh delivers step-by-step recipes and frsh, preportioned ingredients"
- },
- {
+    description:
+      "Our meal delivery service allows you to skip grocery shopping, HelloFresh delivers step-by-step recipes and frsh, preportioned ingredients",
+  },
+  {
     index: "03",
     title: "Why choose ReactMeals as your meal kit delivery service?",
-    description: "Our meal delivery service allows you to skip grocery shopping, HelloFresh delivers step-by-step recipes and frsh, preportioned ingredients"
- },
- {
+    description:
+      "Our meal delivery service allows you to skip grocery shopping, HelloFresh delivers step-by-step recipes and frsh, preportioned ingredients",
+  },
+  {
     index: "04",
     title: "How many times a week does ReactMeals deliver?",
-    description: "Our meal delivery service allows you to skip grocery shopping, HelloFresh delivers step-by-step recipes and frsh, preportioned ingredients"
- }]
+    description:
+      "Our meal delivery service allows you to skip grocery shopping, HelloFresh delivers step-by-step recipes and frsh, preportioned ingredients",
+  },
+];

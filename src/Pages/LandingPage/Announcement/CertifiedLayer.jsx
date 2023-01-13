a3d4fccf1e8e09235982f38fdf07f4d7f6cb5394
KobiManuel <Emmakobi91@gmail.com>
2023-01-13 10:31:49 +0100

    feat(New import): imported pepper from images folder

--- src/Pages/LandingPage/Announcement/CertifiedLayer.jsx
+++ src/Pages/LandingPage/Announcement/CertifiedLayer.jsx
@@ -4,6 +4,7 @@ import styles from '../../../UI/Button/TransparentBtn.module.css'
 import bowl from './images/true_transparent.png'
 import plant from './images/firstPlant.png'
 import signature from './images/signature.png'
+import pepper from './images/pepper.png'
 const CertifiedLayer = () => {
   return (
     <div  className='w-full relative py-16'>
@@ -15,7 +16,7 @@ const CertifiedLayer = () => {
         <div className=' body-font font-poppins flex items-center justify-end col-span-4'>
             <div className=' flex flex-col gap-5'>
           <header className='flex flex-col gap-4'>
-            <h3 className='text-4xl text-[var
+            <h3 className='text-[33px] text-[var
             (--secondary)] font-bold' >We Deliver Anywhere in the Tri-State Area</h3>
             <p className='text-[#868e96] text-sm'>Each React Meal is perfectly sized for one person to enjoy at 1 sitting. Our fully-prepared meals are delivered fresh, and ready to eat in 3 minutes</p>
           </header>

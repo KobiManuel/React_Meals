5e47fb0365cd756866fd2bf5ad4da3b2726b2aa5
KobiManuel <Emmakobi91@gmail.com>
2023-01-13 23:28:13 +0100

    style(User-interface): added display:flex

--- src/Pages/LandingPage/FaQs/Accordion.jsx
+++ src/Pages/LandingPage/FaQs/Accordion.jsx
@@ -3,9 +3,9 @@ import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
 const Accordion = () => {
   return (
-    <div className=' py-14 px-9 flex flex-row w-fit h-fit bg-stone-100 hover:bg-[var(--secondary)] hover:rounderd-[2rem]'>
-        <span>{index}</span>
-        <div>
+    <div className='body-font font-poppins py-14 px-9 flex flex-row justify-center items-center w-fit h-fit bg-stone-100 hover:bg-[var(--secondary)] hover:rounderd-[2rem]'>
+        <span className=' text-4xl text-gray-300 font-semibold'>{index}</span>
+        <div className=' flex flex-col gap-6'>
             <h5>{title}</h5>
             <p>{description}</p>
         </div>

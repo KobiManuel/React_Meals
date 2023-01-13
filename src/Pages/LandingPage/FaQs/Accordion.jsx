ff508af610a4a2255c8e95ab618cfe754e5bee00
KobiManuel <Emmakobi91@gmail.com>
2023-01-13 23:52:53 +0100

    style(formatting): formatted document

--- src/Pages/LandingPage/FaQs/Accordion.jsx
+++ src/Pages/LandingPage/FaQs/Accordion.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
-const Accordion = () => {
+const Accordion = ({ index, title, description }) => {
+    const [open, setOpen] = useState(false);
+    const openHandler = () => {
+        setOpen(!false)
+    }
   return (
-    <div className="body-font font-poppins py-14 px-9 flex flex-row justify-center items-center w-fit h-fit bg-stone-100 hover:bg-[var(--secondary)] hover:rounderd-[2rem]">
+    <div onClick={openHandler} className="body-font font-poppins py-14 px-9 flex flex-row justify-center items-center w-fit h-fit bg-stone-100 hover:bg-[var(--secondary)] hover:rounderd-[2rem]">
       <span className=" text-4xl text-gray-300 font-semibold">{index}</span>
       <div className=" flex flex-col gap-6">
-        <h5>{title}</h5>
-        <p>{description}</p>
+        <h5 className="text-[var(--primary)] text-3xl hover:text-white font-semibold">{title}</h5>
+        <p className=" text-gray-300 text-sm">{description}</p>
       </div>
-      <span>
+      <span className="w-[50px] h-[50px] rounded-full bg-white text-center">
         {!open ? <AiOutlineMinus size={25} /> : <AiOutlinePlus size={25} />}
       </span>
     </div>
--- src/Pages/LandingPage/FaQs/Main.jsx
+++ src/Pages/LandingPage/FaQs/Main.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import Button from '../../../UI/Button/Button'
-import Wrapper from '../../../UI/Wrapper/Wrapper'
+import React from "react";
+import Button from "../../../UI/Button/Button";
+import Wrapper from "../../../UI/Wrapper/Wrapper";
 
 const FaQs = () => {
   return (
     <Wrapper bg_color="bg-white">
-        <div className=' max-w-[1100px] mx-auto px-12 body-font font-poppins'>
-      <header className='flex flex-row justify-between items-center gap-[9rem]'>
-        <h3 className='capitalize text-4xl text-[var(--secondary)] font-semibold '>frequently asked questions</h3>
-        <p className=' text-sm text-gray-400'>Check out frequently asked questions for answers</p>
-        <Button>Contact us</Button>
-      </header>
-        </div>
+      <div className=" max-w-[1100px] mx-auto px-12 body-font font-poppins">
+        <header className="flex flex-row justify-between items-center gap-[9rem]">
+          <h3 className="capitalize text-4xl text-[var(--secondary)] font-semibold ">
+            frequently asked questions
+          </h3>
+          <p className=" text-sm text-gray-400">
+            Check out frequently asked questions for answers
+          </p>
+          <Button>Contact us</Button>
+        </header>
+      </div>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default FaQs
+export default FaQs;

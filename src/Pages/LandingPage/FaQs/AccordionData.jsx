a404e74a9af6b1d399505aa94616075b6d1e12b2
KobiManuel <Emmakobi91@gmail.com>
2023-01-13 22:27:17 +0100

    feat(User-interface): finished populating AccordionData component

--- src/Pages/LandingPage/FaQs/AccordionData.jsx
+++ src/Pages/LandingPage/FaQs/AccordionData.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
-const AccordionData = () => {
-  return (
-    <div>AccordionData</div>
-  )
-}
-
-export default AccordionData
+export const AccordionData = [{
+   index: "01",
+   title: "How does ReactMeals kit delivery service work?",
+   description: "Our meal delivery service allows you to skip grocery shopping, HelloFresh delivers step-by-step recipes and frsh, preportioned ingredients"
+},
+{
+    index: "02",
+    title: "How much do our meals cost?",
+    description: "Our meal delivery service allows you to skip grocery shopping, HelloFresh delivers step-by-step recipes and frsh, preportioned ingredients"
+ },
+ {
+    index: "03",
+    title: "Why choose ReactMeals as your meal kit delivery service?",
+    description: "Our meal delivery service allows you to skip grocery shopping, HelloFresh delivers step-by-step recipes and frsh, preportioned ingredients"
+ },
+ {
+    index: "04",
+    title: "How many times a week does ReactMeals deliver?",
+    description: "Our meal delivery service allows you to skip grocery shopping, HelloFresh delivers step-by-step recipes and frsh, preportioned ingredients"
+ }]

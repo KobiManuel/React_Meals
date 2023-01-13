a6f611318a42a5b12b9efb32eb99da1027d33426
KobiManuel <Emmakobi91@gmail.com>
2023-01-13 23:20:12 +0100

    feat(New import): imported react-icons

--- src/Pages/LandingPage/FaQs/Accordion.jsx
+++ src/Pages/LandingPage/FaQs/Accordion.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
 const Accordion = () => {
   return (
-    <div>
-        
+    <div className=' py-14 px-9 flex flex-row w-fit h-fit bg-stone-100 hover:bg-[var(--secondary)] hover:rounderd-[2rem]'>
+        <span>{index}</span>
+        <div>
+            <h5>{title}</h5>
+            <p>{description}</p>
+        </div>
+        <span>
+            {!open? (
+                <AiOutlineMinus size={25} />
+            ) : (
+                <AiOutlinePlus size={25}/>
+            )}
+        </span>
     </div>
   )
 }

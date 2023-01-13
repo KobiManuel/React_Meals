70cb9f07d40468c79f382239ac38b8c48b0dc102
KobiManuel <Emmakobi91@gmail.com>
2023-01-13 19:35:11 +0100

    style(User-interface): finished styling upper layer of FaQs section

--- src/Pages/LandingPage/FaQs/Main.jsx
+++ src/Pages/LandingPage/FaQs/Main.jsx
@@ -6,8 +6,8 @@ const FaQs = () => {
   return (
     <Wrapper bg_color="bg-white">
         <div className=' max-w-[1100px] mx-auto px-12 body-font font-poppins'>
-      <header className='flex flex-row justify-between'>
-        <h3 className=' text-4xl text-[var(--secondary)] font-semibold '>frequently asked questions</h3>
+      <header className='flex flex-row justify-between items-center gap-[9rem]'>
+        <h3 className='capitalize text-4xl text-[var(--secondary)] font-semibold '>frequently asked questions</h3>
         <p className=' text-sm text-gray-400'>Check out frequently asked questions for answers</p>
         <Button>Contact us</Button>
       </header>

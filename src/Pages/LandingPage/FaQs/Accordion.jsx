8fc5a1ceaf2d8a13fb926615fa58e74a6589b87a
KobiManuel <Emmakobi91@gmail.com>
2023-01-14 01:03:46 +0100

    feat(formatting): formatted document

--- src/Pages/LandingPage/FaQs/Accordion.jsx
+++ src/Pages/LandingPage/FaQs/Accordion.jsx
@@ -1,20 +1,41 @@
-import React, {useState}from "react";
+import React, { useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
 const Accordion = ({ index, title, description }) => {
-    const [open, setOpen] = useState(false);
-    const openHandler = (index) => {
-        setOpen(!false)
-        if (open === !false) {
-         setOpen(false)
-        }
+  const [open, setOpen] = useState(false);
+  const openHandler = (index) => {
+    setOpen(!false);
+    if (open === !false) {
+      setOpen(false);
     }
+  };
   return (
-    <div onClick={() => openHandler(index) } className="body-font font-poppins py-9 px-9 flex flex-row  justify-between w-full h-fit bg-stone-100 hover:bg-black hover:rounded-[2rem] border-b-[1px] border-b-gray-300 first:rounded-t-3xl last:rounded-b-3xl">
+    <div
+      onClick={() => openHandler(index)}
+      className={
+        !open
+          ? "body-font font-poppins py-9 px-9 flex flex-row  justify-between w-full h-fit bg-stone-100 hover:bg-black hover:rounded-[2rem] border-b-[1px] border-b-gray-300 first:rounded-t-3xl last:rounded-b-3xl"
+          : "body-font font-poppins py-9 px-9 flex flex-row  justify-between w-full h-fit bg-black hover:rounded-[2rem] border-b-[1px] border-b-gray-300 first:rounded-t-3xl last:rounded-b-3xl"
+      }
+    >
       <span className=" text-4xl text-gray-300 font-semibold">{index}</span>
       <div className=" flex flex-col gap-6 w-[500px]">
-        <h5 className={!open ? "text-[var(--secondary)] text-2xl font-semibold hover:text-white" : "text-white text-2xl font-semibold hover:text-white" }>{title}</h5>
-        <p className={!open? "text-gray-300 text-sm hidden" : "text-gray-300 text-sm"}>{description}</p>
+        <h5
+          className={
+            !open
+              ? "text-[var(--secondary)] text-2xl font-semibold hover:text-white"
+              : "text-white text-2xl font-semibold hover:text-white"
+          }
+        >
+          {title}
+        </h5>
+        <p
+          className={
+            !open ? "text-gray-300 text-sm hidden" : "text-gray-300 text-sm"
+          }
+        >
+          {description}
+        </p>
       </div>
       <span className="w-[50px] h-[50px] rounded-full bg-white flex items-center justify-center">
         {!open ? <AiOutlinePlus size={25} /> : <AiOutlineMinus size={25} />}
--- src/Pages/LandingPage/FaQs/Main.jsx
+++ src/Pages/LandingPage/FaQs/Main.jsx
@@ -7,7 +7,7 @@ import { AccordionData } from "./AccordionData";
 const FaQs = () => {
   return (
     <Wrapper bg_color="bg-white">
-      <div className=" max-w-[1100px] mx-auto px-12 body-font font-poppins flex flex-col gap-12">
+      <div className=" max-w-[1100px] mx-auto px-12 body-font font-poppins flex flex-col gap-[5rem]">
         <header className="flex flex-row justify-between items-center gap-[9rem]">
           <h3 className="capitalize text-4xl text-[var(--secondary)] font-semibold ">
             frequently asked questions

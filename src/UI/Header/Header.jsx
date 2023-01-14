64c5de6374a7148a41f07b43b6c42cf58839cf60
KobiManuel <Emmakobi91@gmail.com>
2023-01-14 12:27:42 +0100

    style(formatting): formatted Header.jsx

--- src/UI/Header/Header.jsx
+++ src/UI/Header/Header.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import dash from '../../assets/dash2.png'
+import React from "react";
+import dash from "../../assets/dash2.png";
 
-const Header = ({ title,text_color, position }) => {
+const Header = ({ title, text_color, position }) => {
   return (
-    <header className={`${position} capitalize body-font font-poppins text-center w-fit mx-auto`}>
-    <h5 className={`${text_color} text-4xl font-semibold`}>
-      {title}
-    </h5>
-    <img src={dash} alt="/" className="w-[250px] h-[23px]  mx-auto" />
-  </header>
-  )
-}
+    <header
+      className={`${position} capitalize body-font font-poppins text-center w-fit mx-auto`}
+    >
+      <h5 className={`${text_color} text-4xl font-semibold`}>{title}</h5>
+      <img src={dash} alt="/" className="w-[250px] h-[23px]  mx-auto" />
+    </header>
+  );
+};
 /* w-[300px] h-[100px] */
-export default Header
+export default Header;

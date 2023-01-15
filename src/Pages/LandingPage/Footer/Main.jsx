dae33824fa7926a93259910420f4935fde8af846
KobiManuel <Emmakobi91@gmail.com>
2023-01-15 21:15:44 +0100

    feat(New import): imported lastPlant.png

--- src/Pages/LandingPage/Footer/Main.jsx
+++ src/Pages/LandingPage/Footer/Main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import MainFooter from './MainFooter'
+import plant2 from './images/lastPlant.png'
 
 const Footer = () => {
   return (
     <div>
+        <img src={plant2} alt="/" />
         <MainFooter />
     </div>
   )

6d63d14ab5b1ebe99d92711f6e7a2f9993f69090
KobiManuel <Emmakobi91@gmail.com>
2023-01-15 22:37:47 +0100

    style(formatting): formatted MainFooter

--- src/Pages/LandingPage/Footer/Main.jsx
+++ src/Pages/LandingPage/Footer/Main.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import MainFooter from './MainFooter'
-import plant2 from './images/lastPlant.png'
-import FooterFloat from './FooterFloat'
+import React from "react";
+import MainFooter from "./MainFooter";
+import plant2 from "./images/lastPlant.png";
+import FooterFloat from "./FooterFloat";
 
 const Footer = () => {
   return (
-    <div className='relative'>
-        <img src={plant2} alt="/" className='w-[200px] object-cover relative left-[-20px] ' />
-        <MainFooter />
-        <FooterFloat />
+    <div className="relative">
+      <img
+        src={plant2}
+        alt="/"
+        className="w-[200px] object-cover relative left-[-20px] "
+      />
+      <MainFooter />
+      <FooterFloat />
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;

2df4da83129e6eb95e82ef2e66a94f7bd3d17f20
KobiManuel <Emmakobi91@gmail.com>
2023-01-15 19:31:03 +0100

    style(User-interface): added some positioning styling

--- src/Pages/LandingPage/Footer/MainFooter.jsx
+++ src/Pages/LandingPage/Footer/MainFooter.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
-import Wrapper from '../../../UI/Wrapper/Wrapper'
 import footer_bg from '../TopSection/images/Hero2.jpg'
 
 const MainFooter = () => {
   return (
-    <Wrapper  style={{
+    <div className='relative'>
+    <div style={{
         backgroundImage: "url(" + footer_bg + ")",
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center center",
+        backgroundAttachment: "fixed",
         width: "100%",
         maxWidth: "100%",
-        height: "500px",
-        filter: "brightness(0.30)"
-      }}>
-       
-    </Wrapper>
+        height: "500px"
+      }} className="absolute before:content-[''] before:absolute before:brightness-[0.30] z-[-1]"> </div>
+       <div className=' max-w-[1100px] mx-auto body-font font-poppins py-[96px]'>
+           <div className=' grid grid-cols-5 justify-center items-center'>
+           <div className="pl-2 w-fit flex flex-row">
+          <h3 className=" text-2xl font-semibold text-[var(--primary)]">
+            React <span className="text-white">Meals</span>
+          </h3>
+        </div>
+
+           </div>
+       </div>
+    </div>
   )
 }
 

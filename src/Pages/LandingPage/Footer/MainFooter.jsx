cc3ee6790f1405f178b28ae9f36b58f43c14331e
KobiManuel <Emmakobi91@gmail.com>
2023-01-15 19:49:09 +0100

    feat(User-interface): finished styling first grid column

--- src/Pages/LandingPage/Footer/MainFooter.jsx
+++ src/Pages/LandingPage/Footer/MainFooter.jsx
@@ -16,12 +16,21 @@ const MainFooter = () => {
       }} className="absolute before:content-[''] before:absolute before:brightness-[0.30] z-[-1]"> </div>
        <div className=' max-w-[1100px] mx-auto body-font font-poppins py-[96px]'>
            <div className=' grid grid-cols-5 justify-center items-center'>
+            <header className=' flex flex-col gap-8'>
            <div className="pl-2 w-fit flex flex-row">
           <h3 className=" text-2xl font-semibold text-[var(--primary)]">
             React <span className="text-white">Meals</span>
           </h3>
         </div>
-
+          <div className='pl-2 flex flex-col gap-6'>
+            <p className=' text-stone-300 text-xs'>The fictional Address of the React Meals Kit made up in KobiLand</p>
+            <div className='flex flex-row gap-5'>
+                <span className=' w-[40px] h-[40px] rounded-full bg-white'></span>
+                <span  className=' w-[40px] h-[40px] rounded-full  bg-white'></span>
+                <span  className=' w-[40px] h-[40px] rounded-full bg-white'></span>
+            </div>
+          </div>
+             </header>
            </div>
        </div>
     </div>

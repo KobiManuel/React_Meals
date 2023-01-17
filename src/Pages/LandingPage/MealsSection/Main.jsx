34fbddc2669052d7d6240c5f333b4e39c6f70b2a
KobiManuel <Emmakobi91@gmail.com>
2023-01-17 23:43:26 +0100

    style(formatting): formatted document

--- src/Pages/LandingPage/MealsSection/Main.jsx
+++ src/Pages/LandingPage/MealsSection/Main.jsx
@@ -38,13 +38,21 @@ const MealsSection = () => {
     <Wrapper bg_color=" bg-white z-10">
       <div className=" max-w-[1100px] mx-auto px-16">
         <figure className="w-fit relative -translate-y-[35%] translate-x-6 !mr-0 max-[846px]:-translate-y-[65%] max-[846px]:-translate-x-9 max-[571px]:-translate-y-[95%] max-[571px]:-translate-x-[4.5rem] ">
-          <img src={tomato} alt="/" className=" w-[220px] max-[571px]:w-[150px]" />
+          <img
+            src={tomato}
+            alt="/"
+            className=" w-[220px] max-[571px]:w-[150px]"
+          />
         </figure>
         <header className="capitalize body-font font-poppins text-center w-fit mx-auto -translate-y-[100%]  max-[462px]:mb-9 ">
           <h5 className="text-4xl text-[var(--secondary)] font-semibold  max-[410px]:text-2xl">
             Favorite meals
           </h5>
-          <img src={dash} alt="/" className="w-[300px] h-[60px] mt-[-9px] max-[410px]:w-[170px] max-[410px]:mt-[-18px]" />
+          <img
+            src={dash}
+            alt="/"
+            className="w-[300px] h-[60px] mt-[-9px] max-[410px]:w-[170px] max-[410px]:mt-[-18px]"
+          />
         </header>
         <div className="w-full">
           <Btn
@@ -56,16 +64,18 @@ const MealsSection = () => {
         <div className="grid grid-cols-4 gap-6 max-[1029px]:grid-cols-3 max-[812px]:grid-cols-2 max-[618px]:grid-cols-1">
           {item
             .slice(0, visible)
-            .map(({ meal, mealNameTop, mealNameBottom, id, onClick, price }) => (
-              <MealCard
-                meal={meal}
-                mealNametop={mealNameTop}
-                mealNameBottom={mealNameBottom}
-                key={id}
-                onClick={changeContent}
-                price={price}
-              />
-            ))}
+            .map(
+              ({ meal, mealNameTop, mealNameBottom, id, onClick, price }) => (
+                <MealCard
+                  meal={meal}
+                  mealNametop={mealNameTop}
+                  mealNameBottom={mealNameBottom}
+                  key={id}
+                  onClick={changeContent}
+                  price={price}
+                />
+              )
+            )}
         </div>
         {item === MealCardInfo ? (
           <div className=" w-fit mx-auto mt-12">

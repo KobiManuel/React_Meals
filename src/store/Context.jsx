f22f93eb5f4cd3332b5deba1699865e2056ffd06
KobiManuel <Emmakobi91@gmail.com>
2023-01-22 23:19:53 +0100

    feat: added functionality for adding and removing from cart

--- src/store/Context.jsx
+++ src/store/Context.jsx
@@ -1,13 +1,22 @@
-import React, { createContext } from 'react'
+import React, { createContext, useContext, useReducer } from 'react'
+import { MealCardInfo } from '../Pages/LandingPage/MealsSection/MealCardInfo';
+import { cartReducer } from './Reducers';
 
 const Cart = createContext();
 
 const Context = ({children}) => {
+    const [state, dispatch] = useReducer(cartReducer, {
+        products: MealCardInfo,
+        cart:[]
+    })
   return (
-    <Cart.Provider>
+    <Cart.Provider value={{state, dispatch}}>
     {children}
     </Cart.Provider>
   )
 }
 
 export default Context
+export const CartState = () => {
+    return useContext(Cart);
+}

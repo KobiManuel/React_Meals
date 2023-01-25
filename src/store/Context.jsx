43ca1139f757db7776d63a7cb77090a8ed5e539a
KobiManuel <Emmakobi91@gmail.com>
2023-01-25 23:47:54 +0100

    style: formatted document

--- src/store/Context.jsx
+++ src/store/Context.jsx
@@ -1,22 +1,18 @@
-import React, { createContext, useContext, useReducer } from 'react'
-import { MealCardInfo } from '../Pages/LandingPage/MealsSection/MealCardInfo';
-import { cartReducer } from './Reducers';
+import React, { createContext, useContext, useReducer } from "react";
+import { MealCardInfo } from "../Pages/LandingPage/MealsSection/MealCardInfo";
+import { cartReducer } from "./Reducers";
 
 const Cart = createContext();
 
-const Context = ({children}) => {
-    const [state, dispatch] = useReducer(cartReducer, {
-        products: MealCardInfo,
-        cart:[]
-    })
-  return (
-    <Cart.Provider value={{state, dispatch}}>
-    {children}
-    </Cart.Provider>
-  )
-}
+const Context = ({ children }) => {
+  const [state, dispatch] = useReducer(cartReducer, {
+    products: MealCardInfo,
+    cart: [],
+  });
+  return <Cart.Provider value={{ state, dispatch }}>{children}</Cart.Provider>;
+};
 
-export default Context
+export default Context;
 export const CartState = () => {
-    return useContext(Cart);
-}
+  return useContext(Cart);
+};

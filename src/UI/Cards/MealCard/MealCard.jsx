3c5bac4e573d114c85ffda4e73e6af051322d1ff
KobiManuel <Emmakobi91@gmail.com>
2023-01-29 17:17:31 +0100

    refactor(User-interface): removed props destructuring in MealCard component

--- src/UI/Cards/MealCard/MealCard.jsx
+++ src/UI/Cards/MealCard/MealCard.jsx
@@ -2,14 +2,7 @@ import React from "react";
 import { CartState } from "../../../store/Context";
 import styles from "../../Button/Button.module.css";
 
-const MealCard = ({
-  meal,
-  mealNametop,
-  mealNameBottom,
-  id,
-  onClick,
-  price,
-}) => {
+const MealCard = ({ meals }) => {
   const {
     state: { cart },
     dispatch,
@@ -19,18 +12,18 @@ const MealCard = ({
       <div className="relative overflow-hidden w-full h-fit group">
         <div className="w-[100%] h-[220.06px] max-[543px]:h-[150px] max-[543px]:w-[40%]  max-[1029px]:w-[60%] max-[1029px]:h-[150px] mx-auto max-[618px]:w-[50%] max-[618px]:h-[220px]">
           <img
-            src={meal}
+            src={meals.meal}
             alt="/"
             className="mx-auto w-full h-full object-cover "
           />{" "}
         </div>
         <div className="capitalize body-font font-poppins font-semibold text-[var(--secondary)] text-xl text-center">
-          <p>{mealNametop}</p>
-          <p>{mealNameBottom}</p>
+          <p>{meals.mealNametop}</p>
+          <p>{meals.mealNameBottom}</p>
         </div>
         <div className="absolute h-full w-full bg-black/20 flex flex-col items-center justify-center -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300">
           <span className="text-lg font-semibold body-font font-poppins text-white ">
-            {price}
+            {meals.price}
           </span>
           <button
             className={`${styles.button} `}

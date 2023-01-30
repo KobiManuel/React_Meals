6fbcd55705a2f33d0e543639e2870378f1101f01
KobiManuel <Emmakobi91@gmail.com>
2023-01-30 07:38:48 +0100

    feat: formatted dcoument

--- src/UI/Cards/MealCard/MealCard.jsx
+++ src/UI/Cards/MealCard/MealCard.jsx
@@ -10,7 +10,7 @@ const MealCard = ({ meals }) => {
 
   return (
     <div className="rounded-[2rem] bg-stone-100 w-[220] h-[334.67px] flex flex-col justify-center items-center">
-      <div className="relative overflow-hidden w-full h-fit group">
+      <div className="relative overflow-hidden w-full h-full rounded-[2rem] group">
         <div className="w-[100%] h-[220.06px] max-[543px]:h-[150px] max-[543px]:w-[40%]  max-[1029px]:w-[60%] max-[1029px]:h-[150px] mx-auto max-[618px]:w-[50%] max-[618px]:h-[220px]">
           <img
             src={meals.meal}
@@ -19,7 +19,7 @@ const MealCard = ({ meals }) => {
           />{" "}
         </div>
         <div className="capitalize body-font font-poppins font-semibold text-[var(--secondary)] text-xl text-center">
-          <p>{meals.mealNametop}</p>
+          <p>{meals.mealNameTop}</p>
           <p>{meals.mealNameBottom}</p>
         </div>
         <div className="absolute h-full w-full bg-black/20 flex flex-col items-center justify-center -bottom-10 group-hover:bottom-0 opacity-0 group-hover:opacity-100 transition-all duration-300">

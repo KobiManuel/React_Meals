5ff32c37dbe92eda83a7bf8346725dbf3bcd446a
KobiManuel <Emmakobi91@gmail.com>
2023-02-11 23:19:10 +0100

    feat: formatted document

--- src/Pages/Cart/CartItem.jsx
+++ src/Pages/Cart/CartItem.jsx
@@ -1,22 +1,22 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { CartState } from "../../store/Context";
 
 const CartItem = ({ meals }) => {
-    const [quantity, setQuantity] = useState(1)
-    const {
-        state: { cart },
-        setTotal,
-      } = CartState();
+  const [quantity, setQuantity] = useState(1);
+  const {
+    state: { cart },
+    setTotal,
+  } = CartState();
 
-    //   useEffect(() => {
-    //     setTotal(
-    //       cart.reduce(
-    //         (accumulator, currentValue) => accumulator + Number(Math.round(currentValue.price)),
-    //         0
-    //       )
-    //     );
-    //   }, [cart, quantity, setTotal]);
-    
+  //   useEffect(() => {
+  //     setTotal(
+  //       cart.reduce(
+  //         (accumulator, currentValue) => accumulator + Number(Math.round(currentValue.price)),
+  //         0
+  //       )
+  //     );
+  //   }, [cart, quantity, setTotal]);
+
   return (
     <div>
       <div className="flex justify-between items-center mt-6 pt-6 border-b  border-b-gray-200">
@@ -33,30 +33,44 @@ const CartItem = ({ meals }) => {
 
         <div className="flex justify-center items-center">
           <div className="pr-8 flex ">
-            <button className="font-semibold" onClick={() => {
-                setQuantity(quantity - 1)
-                if(quantity <= 1) {
-                    setQuantity(quantity)
+            <button
+              className="font-semibold"
+              onClick={() => {
+                setQuantity(quantity - 1);
+                if (quantity <= 1) {
+                  setQuantity(quantity);
                 }
-            }}>-</button>
+              }}
+            >
+              -
+            </button>
             <input
               type="text"
               className="focus:outline-none bg-gray-100 border h-6 w-8 rounded text-sm px-2 mx-2"
               value={quantity}
             />
-            <button className="font-semibold" onClick={() => {
-                setQuantity((prevValue) => prevValue + 1) 
+            <button
+              className="font-semibold"
+              onClick={() => {
+                setQuantity((prevValue) => prevValue + 1);
                 setTotal(
-                    cart.reduce(
-                      (accumulator, currentValue) => accumulator + Number(Math.round(currentValue.price))*quantity,
-                      0
-                    )
-                  );
-            }}>+</button>
+                  cart.reduce(
+                    (accumulator, currentValue) =>
+                      accumulator +
+                      Number(Math.round(currentValue.price)) * quantity,
+                    0
+                  )
+                );
+              }}
+            >
+              +
+            </button>
           </div>
 
           <div className="pr-8 ">
-            <span className="text-xs font-medium">${Math.round(meals.price) * quantity}</span>
+            <span className="text-xs font-medium">
+              ${Math.round(meals.price) * quantity}
+            </span>
           </div>
           <div>
             <i className="fa fa-close text-xs font-medium"></i>

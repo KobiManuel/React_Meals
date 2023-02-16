cabb0eb0b6f5538d09588c99026236ef2e435179
KobiManuel <Emmakobi91@gmail.com>
2023-02-16 16:58:33 +0100

    feat(User-interface): justified to start

--- src/Pages/LandingPage/Services/Main.jsx
+++ src/Pages/LandingPage/Services/Main.jsx
@@ -18,7 +18,7 @@ const Services = () => {
     >
       <div className=" max-w-[1100px] mx-auto flex flex-col gap-11">
         <Header title="why react meals?" text_color="text-white" />
-        <div className=" w-full grid grid-cols-3 body-font font-poppins gap-y-20">
+        <div className=" w-full grid lg:grid-cols-3 body-font font-poppins gap-y-20">
           <div className=" flex flex-row text-white  justify-center items-center gap-4">
             <span className="w-[50px] h-[50px] rounded-full bg-white"></span>
             <p className="text-xl font-medium">Easy and Convenient</p>

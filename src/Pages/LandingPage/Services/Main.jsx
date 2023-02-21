fa661bd1966dde100882e4678b86acf8a9bb7784
KobiManuel <Emmakobi91@gmail.com>
2023-02-21 15:18:18 +0100

    feat(Responsiveness): changed initial settings to be visible only on large screens

--- src/Pages/LandingPage/Services/Main.jsx
+++ src/Pages/LandingPage/Services/Main.jsx
@@ -18,28 +18,28 @@ const Services = () => {
     >
       <div className=" max-w-[1100px] mx-auto flex flex-col gap-11">
         <Header title="why react meals?" text_color="text-white" />
-        <div className=" w-full grid lg:grid-cols-3 md:grid-cols-2 body-font font-poppins gap-y-20 justify-center items-center">
-          <div className=" flex flex-row text-white  justify-start pl-9 items-center gap-4">
+        <div className=" w-full grid lg:grid-cols-3 md:grid-cols-2 body-font font-poppins gap-y-20">
+          <div className=" flex flex-row text-white  lg:justify-start lg:pl-9 items-center gap-4">
             <span className="w-[50px] h-[50px] rounded-full bg-white"></span>
             <p className="text-xl font-medium">Easy and Convenient</p>
           </div>
-          <div className=" flex flex-row text-white  justify-start pl-9 items-center gap-4">
+          <div className=" flex flex-row text-white  lg:justify-start lg:pl-9 items-center gap-4">
             <span className="w-[50px] h-[50px] rounded-full bg-white"></span>
             <p className="text-xl font-medium">Kosher Supervision</p>
           </div>
-          <div className=" flex flex-row text-white  justify-start pl-9 items-center gap-4">
+          <div className=" flex flex-row text-white  lg:justify-start lg:pl-9 items-center gap-4">
             <span className="w-[50px] h-[50px] rounded-full bg-white"></span>
             <p className="text-xl font-medium">No Commitment</p>
           </div>
-          <div className=" flex flex-row text-white  justify-start pl-9 items-center gap-4">
+          <div className=" flex flex-row text-white  lg:justify-start lg:pl-9 items-center gap-4">
             <span className="w-[50px] h-[50px] rounded-full bg-white"></span>
             <p className="text-xl font-medium">Most 5-Star Reviews</p>
           </div>
-          <div className=" flex flex-row text-white  justify-start pl-9 items-center gap-4">
+          <div className=" flex flex-row text-white  lg:justify-start lg:pl-9 items-center gap-4">
             <span className="w-[50px] h-[50px] rounded-full bg-white"></span>
             <p className="text-xl font-medium">Fresh and Affordable</p>
           </div>
-          <div className=" flex flex-row text-white  justify-start pl-9 items-center gap-4">
+          <div className=" flex flex-row text-white  lg:justify-start lg:pl-9 items-center gap-4">
             <span className="w-[50px] h-[50px] rounded-full bg-white"></span>
             <p className="text-xl font-medium">No Skimpin Chicken!</p>
           </div>

4af4cad7ad79a2587aa59668f81d1a432a97bb8a
KobiManuel <Emmakobi91@gmail.com>
2023-02-21 17:40:35 +0100

    feat(Responsiveness): made some responsive changes

--- src/Pages/LandingPage/Announcement/CertifiedLayer.jsx
+++ src/Pages/LandingPage/Announcement/CertifiedLayer.jsx
@@ -13,11 +13,11 @@ const CertifiedLayer = () => {
         alt="/"
         className="absolute w-[140px] top-0 left-[-50px]"
       />
-      <div className="grid grid-cols-12 items-center">
-        <div className=" col-span-7">
+      <div className="grid min-[803px]:grid-cols-12 items-center w-fit max-[803px]:grid-cols-1 justify-center mx-auto">
+        <div className=" min-[803px]:col-span-7 max-[803px]:w-[80%] mx-auto">
           <img src={bowl} alt="/" className="w-full h-full" />
         </div>
-        <div className=" body-font font-poppins flex items-center justify-end col-span-4">
+        <div className=" body-font font-poppins flex items-center justify-end min-[803px]:col-span-4 max-[851px]:w-[300px] max-[803px]:px-6 max-[803px]:w-fit">
           <div className=" flex flex-col gap-5">
             <header className="flex flex-col gap-4">
               <h3

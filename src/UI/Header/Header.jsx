2a391a3bc8c0da9b58456efdc610ad7fe1146b16
KobiManuel <Emmakobi91@gmail.com>
2023-02-21 16:51:21 +0100

    feat(User-interface): changed text size

--- src/UI/Header/Header.jsx
+++ src/UI/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = ({ title, text_color, position }) => {
     <header
       className={`${position} capitalize body-font font-poppins text-center w-fit mx-auto`}
     >
-      <h5 className={`${text_color} text-4xl font-semibold`}>{title}</h5>
+      <h5 className={`${text_color} text-4xl font-semibold max-[410px]:text-3xl`}>{title}</h5>
       <img src={dash} alt="/" className="w-[250px] h-[23px]  mx-auto" />
     </header>
   );

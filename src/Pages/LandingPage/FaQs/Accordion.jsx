9ae4ec3c726ca55ed8333bbcb8c133f9f045b518
KobiManuel <Emmakobi91@gmail.com>
2023-02-22 11:38:31 +0100

    feat(Responsiveness): finished responsiveness on FaQs section

--- src/Pages/LandingPage/FaQs/Accordion.jsx
+++ src/Pages/LandingPage/FaQs/Accordion.jsx
@@ -6,31 +6,31 @@ const Accordion = ({ open, toggle, index, title, description, id }) => {
       onClick={toggle}
       className={
         !open
-          ? "group transition-all duration-500 ease-in-out delay-150 body-font font-poppins py-9 px-9 flex flex-row  justify-between w-full h-fit bg-stone-100 hover:bg-black hover:rounded-[2rem] border-b-[1px] border-b-gray-300 first:rounded-t-3xl last:rounded-b-3xl"
+          ? " group transition-all duration-500 ease-in-out delay-150 body-font font-poppins py-9 px-9 flex flex-row  justify-between w-full h-fit bg-stone-100 hover:bg-black hover:rounded-[2rem] border-b-[1px] border-b-gray-300 first:rounded-t-3xl last:rounded-b-3xl"
           : "body-font font-poppins py-9 px-9 flex flex-row  justify-between w-full h-fit bg-black hover:rounded-[2rem] border-b-[1px] border-b-gray-300 first:rounded-t-3xl last:rounded-b-3xl rounded-[2rem] "
       }
     >
-      <span className=" text-4xl text-[#868e96] font-semibold">{id}</span>
-      <div className=" flex flex-col gap-6 w-[500px]">
+      <span className=" text-4xl text-[#868e96] font-semibold max-[826px]:text-2xl">{id}</span>
+      <div className=" flex flex-col gap-6 w-[500px] max-[826px]:w-[400px] max-[692px]:w-[250px] max-[443px]:w-[190px] max-[367px]:w-[150px] ">
         <h5
           className={
             !open
-              ? "text-[var(--secondary)] text-2xl font-semibold hover:text-white group-hover:text-white"
-              : "text-white text-2xl font-semibold hover:text-white"
+              ? "text-[var(--secondary)] text-2xl max-[692px]:text-[15px] max-[826px]:text-1xl font-semibold hover:text-white group-hover:text-white"
+              : "text-white text-2xl max-[692px]:text-[15px] max-[826px]:text-1xl font-semibold hover:text-white"
           }
         >
           {title}
         </h5>
         <p
           className={
-            !open ? "text-gray-300 text-sm hidden" : "text-gray-300 text-sm"
+            !open ? "text-gray-300 text-sm hidden max-[826px]:text-xs" : "text-gray-300 text-sm max-[826px]:text-xs"
           }
         >
           {description}
         </p>
       </div>
-      <span className="w-[50px] h-[50px] rounded-full bg-white flex items-center justify-center">
-        {!open ? <AiOutlinePlus size={25} /> : <AiOutlineMinus size={25} />}
+      <span className="w-[50px] h-[50px] max-[826px]:w-[25px] max-[826px]:h-[25px] rounded-full bg-white flex items-center justify-center">
+        {!open ? <AiOutlinePlus size={25} className=" max-[826px]:w-[15px]" /> : <AiOutlineMinus size={25} className=" max-[826px]:w-[15px]" />}
       </span>
     </div>
   );
--- src/Pages/LandingPage/FaQs/Main.jsx
+++ src/Pages/LandingPage/FaQs/Main.jsx
@@ -19,10 +19,10 @@ const FaQs = () => {
     <Wrapper bg_color="bg-white">
       <div className=" max-w-[1100px] mx-auto px-12 max-[581px]:px-2 body-font font-poppins flex flex-col gap-[5rem]">
         <header className="flex flex-row max-[759px]:flex-col max-[759px]:gap-3 justify-between items-center min-[1042px]:gap-[9rem] max-[1042px]:gap-[5rem] max-[977px]:gap-[2rem] max-[881px]:gap-0">
-          <h3 className="capitalize text-4xl text-[var(--secondary)] font-semibold max-[881px]:w-fit max-[817px]:text-3xl max-[538px]:text-2xl">
+          <h3 className="capitalize max-[373px]:text-center text-4xl text-[var(--secondary)] font-semibold max-[881px]:w-fit max-[817px]:text-3xl max-[538px]:text-2xl">
             frequently asked questions
           </h3>
-          <p className=" text-sm text-[#868e96] font-medium max-[881px]:w-fit ">
+          <p className=" text-sm text-[#868e96] font-medium max-[881px]:w-fit max-[373px]:text-center ">
             Check out frequently asked questions for answers
           </p>
           <Button>Contact us</Button>

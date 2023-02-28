8eab864895fbdb076f80157b75e366822b1a64f2
KobiManuel <Emmakobi91@gmail.com>
2023-02-28 20:28:44 +0100

    style(formatting): formatted document

--- src/Pages/LandingPage/FaQs/Accordion.jsx
+++ src/Pages/LandingPage/FaQs/Accordion.jsx
@@ -10,7 +10,9 @@ const Accordion = ({ open, toggle, index, title, description, id }) => {
           : "body-font font-poppins py-9 px-9 flex flex-row  justify-between w-full h-fit bg-black hover:rounded-[2rem] border-b-[1px] border-b-gray-300 first:rounded-t-3xl last:rounded-b-3xl rounded-[2rem] "
       }
     >
-      <span className=" text-4xl text-[#868e96] font-semibold max-[826px]:text-2xl">{id}</span>
+      <span className=" text-4xl text-[#868e96] font-semibold max-[826px]:text-2xl">
+        {id}
+      </span>
       <div className=" flex flex-col gap-6 w-[500px] max-[826px]:w-[400px] max-[692px]:w-[250px] max-[443px]:w-[190px] max-[367px]:w-[150px] ">
         <h5
           className={
@@ -23,14 +25,20 @@ const Accordion = ({ open, toggle, index, title, description, id }) => {
         </h5>
         <p
           className={
-            !open ? "text-gray-300 text-sm hidden max-[826px]:text-xs" : "text-gray-300 text-sm max-[826px]:text-xs"
+            !open
+              ? "text-gray-300 text-sm hidden max-[826px]:text-xs"
+              : "text-gray-300 text-sm max-[826px]:text-xs"
           }
         >
           {description}
         </p>
       </div>
       <span className="w-[50px] h-[50px] max-[826px]:w-[25px] max-[826px]:h-[25px] rounded-full bg-white flex items-center justify-center">
-        {!open ? <AiOutlinePlus size={25} className=" max-[826px]:w-[15px]" /> : <AiOutlineMinus size={25} className=" max-[826px]:w-[15px]" />}
+        {!open ? (
+          <AiOutlinePlus size={25} className=" max-[826px]:w-[15px]" />
+        ) : (
+          <AiOutlineMinus size={25} className=" max-[826px]:w-[15px]" />
+        )}
       </span>
     </div>
   );
